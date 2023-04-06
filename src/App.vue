053a507614741f0d635b36fb5468a33c1b5e50d7
Michael Goff <michael@goffdrop.com>
2023-04-06 16:37:21 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import TipTap from './components/TipTap.vue';
+
+export default {
+  components: {TipTap},
+};
+</script>
+
+<template>
+  <div id="app">
+    <main>
+      <tip-tap />
+    </main>
+  </div>
+</template>
---
+++ src/components/TipTap.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+      <editor-content :editor="editor" />
+  
+      <div class="character-count" v-if="editor">
+        {{ editor.storage.characterCount.characters() }}/{{ limit }} characters
+        <br>
+        {{ editor.storage.characterCount.words() }} words
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import CharacterCount from '@tiptap/extension-character-count'
+  import Document from '@tiptap/extension-document'
+  import Paragraph from '@tiptap/extension-paragraph'
+  import Text from '@tiptap/extension-text'
+  import { Editor, EditorContent } from '@tiptap/vue-2'
+  
+  export default {
+    components: {
+      EditorContent,
+    },
+    data() {
+      return {
+        editor: null,
+        limit: 280,
+      }
+    },
+    mounted() {
+      this.editor = new Editor({
+        extensions: [
+          Document,
+          Paragraph,
+          Text,
+          CharacterCount,
+        ],
+        content: null,
+        editorProps: {
+            attributes: {
+                class: `v-card v-sheet v-sheet--outlined`,
+            },
+        },
+        onUpdate: () => {
+            console.log('Update Called')
+        }
+      })
+    },
+  
+    beforeUnmount() {
+      this.editor.destroy()
+    },
+  }
+  </script>
+  
+  <style lang="scss">
+  /* Basic editor styles */
+  .ProseMirror {
+    > * + * {
+      margin-top: 0.75em;
+    }
+    height: 300px;
+    width: 500px;
+  }
+  
+  .character-count {
+    margin-top: 1rem;
+    color: #868e96;
+  }
+  </style>
